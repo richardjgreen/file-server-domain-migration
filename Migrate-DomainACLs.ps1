29a67cb89293a5519e90564f17ca02b62b0f0858
Richard J Green <richardjgreen@users.noreply.github.com>
2017-07-19 16:55:29 +0100

    Added Initial Scripts

---
+++ Migrate-DomainACLs.ps1
@@ -0,0 +1,167 @@
+﻿<#
+.SYNOPSIS
+    Script to migrate file server permissions from one domain to another.
+.DESCRIPTION
+    This script takes input parameters for the base path to the file server share, the old domain name and the new domain name.
+    The script will clone the permissions granted to the legacy domain users and groups and add them to the new domain users and groups.
+    Legacy domain permissions are not deleted. This allows co-existence to occur.
+
+    An optional AdminGroup parameter can be specified. When a file or folder is found and the user executing the script has no permissions, we
+    will attempt to forcibly add the specified administrative group to the ACL and then retry the update. This is useful when inheritence has been 
+    disabled and Domain Admins, local Administrators and other management groups have been removed.
+
+    Using the Verbose parameter will display the mapping of all of the identities from source to target domain. It will also report the file or folder
+    being processed to allow you to track the process.
+
+    The legacy domain name should be entered using the NetBIOS domain name.
+.NOTES
+Author:  Richard J Green
+Version: 0.5
+Date:    18th July 2017
+.LINK
+    https://richardjgreen.net
+.EXAMPLE
+    Migrate-DomainACLs.ps1 -SearchBase "D:\Files" -LegacyDomain "DomainA" -NewDomain "DomainB"
+    Migrate-DomainACLs.ps1 -SearchBase "D:\Files" -LegacyDomain "DomainA" -NewDomain "DomainB" -AdminGroup "Domain Admins" -Verbose
+#>
+
+Param(
+    # Parameter for the Search Base. This is the root folder path.
+    [Parameter(Mandatory=$true)]
+    [string]$SearchBase,
+    # Parameter for the legacy domain name.
+    [Parameter(Mandatory=$true)]
+    [regex]$LegacyDomain,
+    # Parameter for the new domain name.
+    [Parameter(Mandatory=$true)]
+    [string]$NewDomain,
+    # Optional parameter for the administrative group name in the new domain.
+    [string]$AdminGroup
+)
+
+# Map the input parameters to objects.
+$basePath = $SearchBase
+$oldDomain = "$LegacyDomain\*"
+$newDomainGroup = "$NewDomain\$AdminGroup"
+
+# Import the NTFS Security PowerShell Module
+Try {
+    Import-Module NTFSSecurity
+} Catch {
+    Write-Error "The NTFS Security PowerShell Module could not be imported. Please check that it is installed."
+    Exit
+}
+
+#region Top-Level Folder
+
+# Get the top-level folder only.
+$topFolder = Get-Item -Path $basePath
+
+# Get the ACL on the top-level folder.
+$topFolderPath = $topFolder.FullName
+$acl = $topFolder | Get-NTFSAccess -ExcludeInherited 
+
+ForEach ($ace in $acl) {
+
+    # Parse the ACEs for those containing the legacy domain name.
+    If ($ace.Account.AccountName -like $oldDomain) {
+        # Capture the account name.
+        $aceOldAccount = $ace.Account.AccountName
+        $aceNewAccount = $LegacyDomain.Replace($aceOldAccount, $NewDomain, 1)
+        $acePermission = $ace.AccessRights
+        
+        # Write an information message to show the old account to new account mapping.
+        Write-Information "Mapped identity $aceOldAccount to $aceNewAccount"
+
+        # Attempt to add the new ACE to the ACL for the object.
+        Try {
+            Add-NTFSAccess -Path $topFolderPath -Account $aceNewAccount -AccessRights $acePermission
+        } Catch {
+            # Attempt to add a new ACE for the specified administrative security group in the new domain.
+            Try {
+                Enable-Privileges
+                Add-NTFSAccess -Path $childPath -Account $newDomainGroup -AccessRights FullControl
+                Disable-Privileges
+                # Retry adding the object ACE to the ACL after applying the admin group.
+                Try {
+                    Add-NTFSAccess -Path $topFolderPath -Account $aceNewAccount -AccessRights $acePermission
+                } Catch {
+                }
+            } Catch {
+                # Output warning if failed to forcibly add the administrative group permission.
+                Write-Warning "Failed to forcibly add $newDomainGroup to the ACL for $childPath."
+            }
+                
+            # Output warning for failed item.
+            Write-Warning "Failed to update ACL for $childPath."
+
+        }
+
+    } Else {
+        # Write a debug message if there was no legacy domain ACE found on the file.
+        Write-Information "No legacy domain permissions found on $topFolderPath."
+    }
+
+}
+
+#endregion
+
+#region Child Folders and Files
+
+# Get all the child folders and files.
+$children = Get-ChildItem -Path $basePath -Recurse
+
+ForEach ($child in $children) {
+    $childPath = $child.FullName
+    $acl = $child | Get-NTFSAccess -ExcludeInherited
+
+    ForEach ($ace in $acl) {
+
+        # Parse the ACEs for those containing the legacy domain name.
+        If ($ace.Account.AccountName -like $oldDomain) {
+            # Capture the account name.
+            $aceOldAccount = $ace.Account.AccountName
+            $aceNewAccount = $LegacyDomain.Replace($aceOldAccount, $NewDomain, 1)
+            $acePermission = $ace.AccessRights
+            $aceType = $ace.AccessControlType
+            $aceInherit = $ace.InheritanceFlags
+            $acePropagation = $ace.PropagationFlags
+        
+            # Write a verbose message to show the old account to new account mapping.
+            Write-Verbose "Mapped identity $aceOldAccount to $aceNewAccount"
+
+            # Write a verbose message to show the working file or folder.
+            Write-Verbose "Updating ACL on $childPath."
+
+            # Attempt to add the new ACE to the ACL for the object.
+            Try {
+                Add-NTFSAccess -Path $childPath -Account $aceNewAccount -AccessRights $acePermission -AccessType $aceType -InheritanceFlags $aceInherit -PropagationFlags $acePropagation
+            } Catch {
+                # Attempt to add a new ACE for the specified administrative security group in the new domain.
+                Try {
+                    Enable-Privileges
+                    Add-NTFSAccess -Path $childPath -Account $newDomainGroup -AccessRights FullControl
+                    Disable-Privileges
+                    # Retry adding the object ACE to the ACL after applying the admin group.
+                    Try {
+                        Add-NTFSAccess -Path $childPath -Account $aceNewAccount -AccessRights $acePermission -AccessType $aceType -InheritanceFlags $aceInherit -PropagationFlags $acePropagation
+                    } Catch {
+                    }
+                } Catch {
+                    # Output warning if failed to forcibly add the administrative group permission.
+                    Write-Warning "Failed to forcibly add $newDomainGroup to the ACL for $childPath."
+                }
+                
+                # Output warning for failed item.
+                Write-Warning "Failed to update ACL for $childPath."
+
+            }
+
+            } Else {
+                # Write a debug message if there was no legacy domain ACE found on the file.
+                Write-Information "No legacy domain permissions found on $childPath."
+        }
+    }
+}
+
+#endregion
---
+++ Remove-LegacyDomainACL.ps1
@@ -0,0 +1,159 @@
+﻿<#
+.SYNOPSIS
+    Script to remove all legacy domain permissions from file server ACLs.
+.DESCRIPTION
+    This script takes input parameters for root path and legacy domain name. Once provided, the script will recursively search
+    the ACL on files and folders in that path for permissions matching the legacy domain name. Once found, they will be removed.
+    Orphaned and unresolvable SIDs will also be purged from the ACL.
+
+    The legacy domain name should be entered using the NetBIOS domain name.
+
+    IMPORTANT: Verify that all configured Active Directory trusts are operational before running this script. If ACEs are present for users or groups
+    in trusted domains and the trust is non-operational, the SID will be unresolvable and therefore removed from the ACL.
+.NOTES
+Author:  Richard J Green
+Version: 0.5
+Date:    18th July 2017
+.LINK
+    https://richardjgreen.net
+.EXAMPLE
+    Remove-LegacyDomainACL.ps1 -SearchBase "D:\Files" -LegacyDomain "DomainA"
+#>
+
+Param(
+    # Parameter for the Search Base. This is the root folder path.
+    [Parameter(Mandatory=$true)]
+    [string]$SearchBase,
+    # Parameter for the legacy domain name.
+    [Parameter(Mandatory=$true)]
+    [string]$LegacyDomain
+)
+
+# Map the input parameters to objects.
+$basePath = $SearchBase
+$domain = "$LegacyDomain\*"
+
+# Import the NTFS Security PowerShell Module
+Try {
+    Import-Module NTFSSecurity
+} Catch {
+    Write-Error "The NTFS Security PowerShell Module could not be imported. Please check that it is installed."
+    Exit
+}
+
+#region Top-Level Folder
+
+# Get the top-level folder only.
+$topFolder = Get-Item -Path $basePath
+
+# Get the ACL on the top-level folder.
+$topFolderPath = $topFolder.FullName
+$acl = $topFolder | Get-NTFSAccess -ExcludeInherited
+
+# Enumerate the ACEs on the top-level folder for legacy domain entries.
+ForEach ($ace in $acl) {
+
+    # Clean up orphaned and unresolvable SIDs.
+    Get-NTFSOrphanedAccess -Path $topFolder | Remove-NTFSAccess
+
+    # If an ACE is found containing the legacy domain name, remove it.
+    If ($ace.Account.AccountName -like $domain) {
+        # Capture the account name.
+        $aceAccount = $ace.Account.AccountName
+
+        Write-Information "Found legacy domain permission on $topFolderPath for user account $aceAccount."
+
+        # Attempt to remove the ACE.
+        Try {
+            Get-NTFSAccess -Path $topFolder -Account $aceAccount -ExcludeInherited | Remove-NTFSAccess
+        } Catch {
+            Write-Warning "Failed to modify the ACE for folder $topFolderPath, for user $aceAccount."
+        }
+    } Else {
+            # Write a debug message if there was no legacy domain ACE found on the file.
+            Write-Information "No legacy domain permissions found on $topFolderPath."
+    }
+}          
+
+#endregion
+
+#region Top-Level Files
+
+# Get the top-level files only.
+$topFiles = Get-ChildItem -Path $basePath -File
+
+# Get the ACL on each of the top-level files.
+ForEach ($topFile in $topFiles) {
+    $topFilePath = $topFile.FullName
+    $acl = $topFile | Get-NTFSAccess -ExcludeInherited
+    
+    # Enumerate the ACEs on the top-level files for legacy domain entries.
+    ForEach ($ace in $acl) {
+
+        # Clean up orphaned and unresolvable SIDs.
+        Get-NTFSOrphanedAccess -Path $topFilePath | Remove-NTFSAccess
+
+        # If an ACE is found containing the legacy domain name, remove it.
+        If ($ace.Account.AccountName -like $domain) {
+            # Capture the account name.
+            $aceAccount = $ace.Account.AccountName
+
+            Write-Information "Found legacy domain permission on $topFilePath for user $aceAccount."
+
+            # Attempt to remove the ACE.
+            Try {
+                Get-NTFSAccess -Path $topFilePath -Account $aceAccount -ExcludeInherited | Remove-NTFSAccess
+            } Catch {
+                Write-Warning "Failed to modify the ACE for file $topFilePath, for user $aceAccount."
+            }
+
+        } Else {
+            # Write a debug message if there was no legacy domain ACE found on the file.
+            Write-Information "No legacy domain permissions found on $topFilePath."
+        }
+    }
+
+}
+
+#endregion
+    
+#region Child Files and Folders
+
+# Get all the child folders and files.
+$children = Get-ChildItem -Path $basePath -Recurse
+
+# Get the ACL on each file and folder.
+ForEach ($child in $children) {
+    $childPath = $child.FullName
+    $acl = $children | Get-NTFSAccess -ExcludeInherited
+
+    # Enumerate the ACEs on the top-level files for legacy domain entries.
+    ForEach ($ace in $acl) {
+
+        # Clean up orphaned and unresolvable SIDs.
+        Get-NTFSOrphanedAccess -Path $childPath | Remove-NTFSAccess
+
+        # If an ACE is found containing the legacy domain name, remove it.
+        If ($ace.Account.AccountName -like $domain) {
+            # Capture the account name.
+            $aceAccount = $ace.Account.AccountName
+
+            Write-Information "Found legacy domain permission on $childPath for user $aceAccount."
+
+            # Attempt to remove the ACE.
+            Try {
+                Get-NTFSAccess -Path $childPath -Account $aceAccount -ExcludeInherited | Remove-NTFSAccess
+            } Catch {
+                Write-Warning "Failed to modify the ACE for file $childPath, for user $aceAccount."
+            }
+
+        } Else {
+            # Write a debug message if there was no legacy domain ACE found on the file.
+            Write-Information "No legacy domain permissions found on $childPath."
+        }
+    }
+
+}
+
+#endregion
+
